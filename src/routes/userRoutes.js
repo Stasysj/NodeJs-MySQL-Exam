fabbff93c78c5c91d34f6464592b351aa2824ce1
Stasys <srasys@st.lt>
2022-05-25 21:22:15 +0300

    login mvc ok

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { getUsers, regUser } = require('../controller/userController');
+const { getUsers, regUser, loginUser } = require('../controller/userController');
 const { validateUser, validateToken } = require('../middleware');
 const { executeDb, getAllUsersDB, registerUserDB, findUserByEmail } = require('../model/userModel');
 const { hashPassword, passWordsMatch, generateJwtToken } = require('../utils/helpers');
@@ -14,31 +14,6 @@ userRoutes.get('/users', validateToken, getUsers);
 // ---------------------------------
 userRoutes.post('/register', validateUser, regUser);
 
-userRoutes.post('/login', validateUser, async (req, res) => {
-  const gautasEmail = req.body.email;
-  const gautasSlaptazodis = req.body.password;
-
-  // patikrinti ar yra toks email kaip gautas
-  const foundUserArr = await findUserByEmail(gautasEmail);
-  // nes findUserByEmail grazina visada masyva
-  const foundUser = foundUserArr[0];
-  console.log('foundUser ===', foundUser);
-  // jei nera 400 email or password not found
-  if (!foundUser) {
-    res.status(400).json('email or password not found (email)');
-    return;
-  }
-  // jei yra tikrinam ar sutampa slaptazodis
-  // bcrypt.compareSync(ivestas slaptazodis, issaugotas hashed slaptazodis)
-  if (!passWordsMatch(gautasSlaptazodis, foundUser.password)) {
-    res.status(400).json('email or password not found (pass)');
-    return;
-  }
-  // sugeneruoti jwt token
-  const payload = { userId: foundUser.id };
-  const token = generateJwtToken(payload);
-  // console.log('token ===', token);
-  res.json({ success: true, token });
-});
+userRoutes.post('/login', validateUser, loginUser);
 // ----------------------
 module.exports = userRoutes;
